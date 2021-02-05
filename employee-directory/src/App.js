5c91be092366665313c9ee742a31948006ebd4e8
Em <emilylauder@gmail.com>
2021-02-05 17:44:57 -0500

    updating fetch and useEffect

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+require("es6-promise").polyfill();
+require("isomorph-fetch");
 
 function App() {
+  const [data, setData] = useState([]);
+  const [q, setQ] = useState("");
+
+  useEffect(() => {
+    fetch()
+      .then((response) => response.json())
+      .then((json) => setData(json));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>filter</div>
+      <div>datatable</div>
     </div>
   );
 }
